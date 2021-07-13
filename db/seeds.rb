72eef0b44b9b054c84d00245fc4125ab8d7e440f
David Jarillo <vjdavid621@gmail.com>
2021-07-13 13:47:25 -0700

    updating seeders and creating devise tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'David', last_name: 'Jarillo', email: 'vjdavid621@gmail.com', password: 'david123!')
